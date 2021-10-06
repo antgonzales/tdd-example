1396d787dcde3df8269fc0c882524e60a1ac1ab5
Anthony Gonzales <antgonzales@icloud.com>
2021-10-05 21:36:49 -0400

    Add loading state when fetching results

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -5,12 +5,15 @@ import './App.css';
 
 function App() {
   const [data, updateData] = React.useState<any>(); 
+  const [status, updateStatus] = React.useState<'idle' | 'loading' | 'success'>('idle');
 
   const onSearchHandler = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    updateStatus('loading');
     try {
       const res = await fetch(`https://swapi.dev/api/people/?search=${e.target.value}`);
       const data = await res.json();
       updateData(data);
+      updateStatus('success');
     } catch (e) {
       console.log(e);
     }
@@ -21,11 +24,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <input type="text" onChange={onSearchHandler} />
-        <ul>
-          {data?.results.map((result: any) => {
+        {status === 'loading' &&
+          <div data-testid="loading-indicator">Loading...</div>
+        }
+        {status === 'success' && <ul>
+          {data?.results?.map((result: any) => {
             return <li key={result.name}>{result.name}</li>
           })}
         </ul>
+        }
       </header>
     </div>
   );
